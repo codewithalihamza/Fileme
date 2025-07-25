45d27eae6a7d0c02491d88e9cf1570c95f9f461b
codewithalihamza <512syedalihamza@gmail.com>
2025-07-26 00:08:42 +0500

    format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Toaster } from "react-hot-toast";
