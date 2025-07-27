82c6b01aca65a55f1bf76aacc70e3adffb27394a
codewithalihamza <512syedalihamza@gmail.com>
2025-07-28 00:40:15 +0500

    format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Toaster } from "sonner";
